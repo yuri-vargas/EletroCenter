f9ce60e0a54f96b36288cb61a83465e6f0b5168a
Yuri <66449065+yuri-vargas@users.noreply.github.com>
2023-10-05 11:17:40 -0300

    att

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,19 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const toggleButton = document.getElementById("toggleMenu");
-  const sideMenu = document.getElementById("sideMenu");
+const menuButton = document.getElementById("menuButton");
+const menu = document.getElementById("menu");
 
-  toggleButton.addEventListener("click", function () {
-    sideMenu.classList.toggle("open");
-  });
+menuButton.addEventListener("click", () => {
+  if (menu.style.left === "-250px") {
+    menu.style.left = "0";
+  } else {
+    menu.style.left = "-250px";
+  }
+});
+
+// Adicione um ouvinte de evento de clique ao documento inteiro
+document.addEventListener("click", (e) => {
+  const target = e.target;
+  if (target !== menuButton && target !== menu && !menu.contains(target)) {
+    // Se o clique não foi no botão do menu, no menu ou em qualquer elemento filho do menu, oculte o menu
+    menu.style.left = "-250px";
+  }
 });
